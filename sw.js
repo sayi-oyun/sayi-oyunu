0749d0bb0b3c9304436ab23cc1999cb5b0d54b3c
sayi-oyun <sasmaz1097@gmail.com>
2026-03-06 21:41:11 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'sayi-tahmin-v1';
+const urlsToCache = [
+  '/sayi-oyunu/',
+  '/sayi-oyunu/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
